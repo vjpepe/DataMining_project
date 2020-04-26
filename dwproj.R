742bf5ae3934e0e805bd00e0a298f0853382e8db
Vincent Pepe <vincentpepe31@gmail.com>
2020-04-26 14:22:13 -0400

    Update dwproj.R

--- dwproj.R
+++ dwproj.R
@@ -12,59 +12,27 @@ library(caTools)
 library(purrr)
 library(data.table)
 library(roll)
+library(party)
 
 # read
 data_url = "https://raw.githubusercontent.com/zzhangusf/Predicting-Fantasy-Football-Points-Using-Machine-Learning/master/data/aggregated_2015.csv"
 df = read.csv(data_url)
 
 # sort by playerID, weeks
-df = df %>% 
-  arrange(playerID, weeks)
-
+df = df %>%
+  arrange(playerID, weeks, Team, Pos)
 tail(df)
 
 # Feature Engineering
 
+
 ######### Game Char indicators
 
+
 ## boolean for home or away game
-df = df %>% 
+
+df = df %>%
   mutate(home = ifelse(h.a == 'h', TRUE, FALSE))
-
-# creating dummies for all opponent teams
-oppts = dummy(df$Oppt, sep = ".")
-
-# helper teams data frame
-teams = data.frame()
-# list of all teams
-team_list = c('ARI', 'ATL', 'BAL', 'BUF', 'CAR', 'CHI', 'CIN', 'CLE', 'DAL', 'DEN', 'DET',
-              'GB', 'HOU', 'IND', 'JAC', 'KC', 'MIA', 'MIN', 'NE', 'NO', 'NYG', 'NYJ',
-              'OAK', 'PHI', 'PIT', 'SD', 'SEA', 'SF', 'STL', 'TB', 'TEN', 'WAS')
-
-tail(df)
-
-# looping through each row and setting 1 to the team
-for(i in 1:length(df$Team)) {
-  temp = ifelse(team_list == df$Team[i], 1, 0)
-  teams = rbind(teams, temp)
-}
-
-#idx = 1:range(length(df$Team))
-rownames(teams) = 1:range(length(df$Team))
-colnames(teams) = team_list
-
-# combining df + oppts + teams
-df = cbind(df, oppts, teams)
-
-# list of all features created so far
-features = c('home', colnames(oppts), team_list)
-
-#### random testing
-testign = df %>% 
-  select(playerID, weeks, name, Oppt.NYG, ATL, BAL) %>% 
-  filter(name == 'K.Cousins')
-
-####### rolling average function
 rolling_average = function(dframe, window) {
   x = runmean(dframe, window)
   x = roll_mean(dframe, width = window, min_obs = 1)
@@ -72,18 +40,72 @@ rolling_average = function(dframe, window) {
   #return(x)
 }
 
-# test = df[ ,c(1,2,9,13)]
+df_test <- df %>%
+  group_by(Oppt, Pos) %>%
+  arrange(weeks) %>%
+  mutate(pref.fd.defense = rolling_average(FD.points, 1)) %>%
+  mutate(roll_avg_def = rolling_average(FD.points, 4))
 
-test2 = df %>% 
-  group_by(playerID) %>% 
-  mutate(test_val2 = rolling_average(FD.points, 4))
+df_test <- df_test %>%
+  group_by(playerID) %>%
+  arrange(weeks) %>%
+  mutate(FD.prev.points = rolling_average(FD.points, 1)) %>%
+  mutate(roll_avg_FD.pts = rolling_average(FD.points, 4))
 
-testing = test2 %>% 
-  select(weeks, playerID, FD.points, test_val2)  %>% 
-  filter(playerID == '00-0031407')
+test <- df_test %>%
+  select(name, playerID, Oppt, Pos, weeks, FD.points, pref.fd.defense, roll_avg_def, FD.prev.points, roll_avg_FD.pts) %>%
+  group_by(Oppt, Pos) %>%
+  arrange(weeks) %>%
+  nest()
+
+quarterback <- df_test %>%
+  filter(Pos == "QB") %>%
+  .[,c(1, 3:10, 14:27, 66, 68:71)]
+
+runningback <- df_test %>%
+  filter(Pos == "RB") %>%
+  .[, c(1, 3:10, 21:44, 66, 68:71)]
+
+wide_receiver <- df_test %>%
+  filter(Pos == "WR") %>%
+  .[, c(1, 3:10, 21:44, 66, 68:71)]
+
+tight_end <- df_test %>%
+  filter(Pos == "TE") %>%
+  .[, c(1, 3:10, 28:34, 66, 68:71)]
+
+kicker <- df_test %>%
+  filter(Pos == "PK") %>%
+  .[, c(1, 3:10, 45:53, 66, 68:71)]
 
 
-##### branch testing #####
+
+# fit the random forest
+q1 <- cforest(FD.points~., data=quarterback, control=cforest_unbiased(mtry=2,ntree=50))
+# get variable importance, based on mean decrease in accuracy
+varimp(q1)
+
+# fit the random forest
+q2 <- cforest(FD.points~., data=runningback, control=cforest_unbiased(mtry=2,ntree=50))
+# get variable importance, based on mean decrease in accuracy
+varimp(q2)
+
+# fit the random forest
+q3 <- cforest(FD.points~., data=wide_receiver, control=cforest_unbiased(mtry=2,ntree=50))
+# get variable importance, based on mean decrease in accuracy
+varimp(q3)
+
+
+# fit the random forest
+q4 <- cforest(FD.points~., data=tight_end, control=cforest_unbiased(mtry=2,ntree=50))
+# get variable importance, based on mean decrease in accuracy
+varimp(q4)
+
+
+# fit the random forest
+q5 <- cforest(FD.points~., data=kicker, control=cforest_unbiased(mtry=2,ntree=50))
+# get variable importance, based on mean decrease in accuracy
+varimp(q5)
 
 
 
